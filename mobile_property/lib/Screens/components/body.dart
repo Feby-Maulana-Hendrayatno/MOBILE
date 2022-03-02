ac2a58de3f1903190d2ce359c3e7b97adeb87388
monarosa22 <monarosanah66@gmail.com>
2022-03-02 22:19:52 +0700

    baru

--- mobile_property/lib/Screens/components/body.dart
+++ mobile_property/lib/Screens/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Welcome/components/background.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -6,16 +8,34 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     // This size provide us total heigh and width of our screen
     return Background(
-      child: Column(children: <Widget>[]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Welcome To Property", 
+            style:TextStyle(fontWeight : FontWeight.bold),
+          ), 
+        SvgPicture.asset(
+          "assets/icons/chat.svg", 
+          height: size.height*0.45,
+        ), 
+        RoundedButton(),
+      ],
+      ),
     );
   }
 }
 
-class Background extends StatelessWidget {
-  final Widget child;
-  const Background({
-    Key key,
-    @required this.child,
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Function press;
+  final Color color, textColor;
+  const RoundedButton({
+    Key key, 
+    this.text, 
+    this.press, 
+    this.color = kPrimaryColor,
+    this.textColor = Colors.white,
   }) : super(key: key);
 
   final Size size;
@@ -24,27 +44,18 @@ class Background extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset("assets/images/main_top.png",
-                width: size.width * 0.3),
+      width: size.width * 0.8,
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(29),
+          child: FlatButton(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+            color: kPrimaryColor,
+            onPressed: () {}, 
+            child: Text(
+              text,
+              style: TextStyle(color: textColor),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_bottom.png",
-              width: size.width * 0.2,
-            ),
-          ),
-          child,
-        ],
+        ),
       ),
     );
   }
