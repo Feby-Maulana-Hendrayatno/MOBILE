4cb931553cac6a741ce5b497cff89697ff21c86b
Feby-Maulana-Hendrayatno <hendrayatno26@gmail.com>
2022-03-07 20:36:25 +0700

    Login

--- 40_VIDIO/Property_Aplication/proyek/android/app/src/main/kotlin/com/example/proyek/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.proyek
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- alan/proyek_3/android/app/src/main/kotlin/com/example/proyek_3/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.proyek_3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
